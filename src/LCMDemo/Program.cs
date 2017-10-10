513b1cd771438faf0b95fdfb0d05a88f1034d448
Niladri28dutta <nild2141@gmail.com>
2017-10-10 21:17:40 +0530

    added LCM code

--- src/LCMDemo/Program.cs
+++ src/LCMDemo/Program.cs
@@ -9,6 +9,44 @@ namespace LCMDemo
     {
         public static void Main(string[] args)
         {
+            long number1 = 0;
+            Console.WriteLine("Enter first number");
+            var num1 = Console.ReadLine();
+            long.TryParse(num1, out number1);
+            long number2 = 0;
+            Console.WriteLine("Enter second number");
+            var num2 = Console.ReadLine();
+            long.TryParse(num2, out number2);
+            if (number1 < number2)
+            {
+                Console.WriteLine("first number should be greater");
+                Console.ReadKey();
+            }
+            else
+            {
+                var result = GenerateLCM(number1, number2);
+                Console.WriteLine($"the LCM of {number1} and {number2} is {result}");
+                Console.ReadKey();
+            }
+        }
+
+        private static long GenerateGCD(long num1, long num2)
+        {
+            while (num2 != 0)
+            {
+                var remainder = num1 % num2;
+                num1 = num2;
+                num2 = remainder;
+            }
+
+            return num1;
+        }
+
+        private static long GenerateLCM(long num1,long num2)
+        {
+            var gcd = GenerateGCD(num1, num2);
+            var lcm = (num1 * num2) / gcd;
+            return lcm;
         }
     }
 }
